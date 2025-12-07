2eff8e59afe9577a18714a90b5c7ea60e5fdb87f
Dmitriy <dmit8815@gmail.com>
2025-12-08 00:10:35 +0300

    Fix sv_maxclients default for single-player AI
    Increased default sv_maxclients from 8 to 64 to accommodate
    single-player AI entities. The game was crashing with
    'Exceeded sv_maxclients (8), unable to create ai_soldier'
    when loading maps with many AI characters.

--- code/server/sv_init.c
+++ code/server/sv_init.c
@@ -262,7 +262,8 @@ SV_BoundMaxClients
 */
 static void SV_BoundMaxClients( int minimum ) {
 	// get the current maxclients value
-	Cvar_Get( "sv_maxclients", "8", 0 );
+	// RealRTCW: increased default from 8 to 64 for single-player AI entities
+	Cvar_Get( "sv_maxclients", "64", 0 );
 
 	sv_maxclients->modified = qfalse;
 
@@ -711,7 +712,7 @@ void SV_SpawnServer( char *server, qboolean killBots ) {
 		if ( g_gametype->integer == 2 ) {
 			if ( sv_maxclients->latchedString ) {
 				// it's been modified, so grab the new value
-				Cvar_Get( "sv_maxclients", "8", 0 );
+				Cvar_Get( "sv_maxclients", "64", 0 );
 			}
 			if ( sv_maxclients->integer < MAX_CLIENTS ) {
 				Cvar_SetValue( "sv_maxclients", MAX_SP_CLIENTS );
@@ -1001,7 +1002,7 @@ void SV_Init (void)
 	sv_mapname = Cvar_Get( "mapname", "nomap", CVAR_SERVERINFO | CVAR_ROM );
 	sv_privateClients = Cvar_Get( "sv_privateClients", "0", CVAR_SERVERINFO );
 	sv_hostname = Cvar_Get( "sv_hostname", "noname", CVAR_SERVERINFO | CVAR_ARCHIVE );
-	sv_maxclients = Cvar_Get( "sv_maxclients", "8", CVAR_SERVERINFO | CVAR_LATCH );
+	sv_maxclients = Cvar_Get( "sv_maxclients", "64", CVAR_SERVERINFO | CVAR_LATCH );
 
 	sv_minRate = Cvar_Get ("sv_minRate", "0", CVAR_ARCHIVE | CVAR_SERVERINFO );
 	sv_maxRate = Cvar_Get( "sv_maxRate", "0", CVAR_ARCHIVE | CVAR_SERVERINFO );
